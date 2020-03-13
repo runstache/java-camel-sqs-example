bbba070152e4241e9ef111f159e8dcba160900af
Larry Shields <shieldsl@upmc.edu>
2020-03-13 12:44:03 -0400

    Project Setup

---
+++ src/main/java/com/lswebworld/camelsqsexample/CamelSqsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lswebworld.camelsqsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSqsExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CamelSqsExampleApplication.class, args);
+  }
+
+}
